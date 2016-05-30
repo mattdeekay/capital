1cd73deae4b15ed4de21d0d109f3ec8801afa816
Matthew Kim <mdkim@rescomp-15-289515.stanford.edu>
2016-05-30 09:07:42 -0700

    Was able to change pk variable name to poid without change in website display

--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,8 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def post_detail(request, poid):
+    post = get_object_or_404(Post, pk=poid)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
